2325f4a25c70c44b4858907a640cf43634358deb
Stanislaw Adaszewski <s.adaszewski@gmail.com>
2021-11-26 21:55:09 +0100

    Can read TPM now, first attempt at autoboot.

--- stand/efi/loader/main.c
+++ stand/efi/loader/main.c
@@ -1215,6 +1215,8 @@ main(int argc, CHAR16 *argv[])
 	}
 #endif
 
+	tpm2_try_autoboot_or_clear_geli_keys();
+
 	interact();			/* doesn't return */
 
 	return (EFI_SUCCESS);		/* keep compiler happy */
