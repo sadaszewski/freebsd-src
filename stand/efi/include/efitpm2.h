b815077b82d1f18c2870a07b5883576a7071ac0d
Stanislaw Adaszewski <s.adaszewski@gmail.com>
2021-11-27 14:03:20 +0100

    Got it to work for the first time.

--- stand/efi/include/efitpm2.h
+++ stand/efi/include/efitpm2.h
@@ -6,6 +6,9 @@
 EFI_STATUS tpm2_init();
 EFI_STATUS tpm2_geli_passphrase_from_efivar();
 void tpm2_try_autoboot_or_clear_geli_keys();
+void tpm2_check_efivars();
+void tpm2_retrieve_passphrase();
+void tpm2_check_passphrase_marker();
 
 #endif
 
