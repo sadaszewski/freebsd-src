16ac58ec464c54bd3757bbbce1392b042a51e633
Stanislaw Adaszewski <s.adaszewski@gmail.com>
2021-11-29 13:26:59 +0100

    Better wiping of env vars, wiping of GELI keys in tpm2cpm.

--- sys/kern/tpm2cpm.c
+++ sys/kern/tpm2cpm.c
@@ -19,6 +19,115 @@
 #include <sys/iov.h>
 #include <crypto/sha2/sha256.h>
 #include <sys/eventhandler.h>
+#include <sys/malloc.h>
+#include <geom/geom.h>
+#include <geom/geom_int.h>
+#include <geom/eli/g_eli.h>
+
+
+static void zero_and_unset_env(const char *name) {
+	char *value = kern_getenv(name);
+	if (value == NULL)
+		return;
+	while (*value) {
+		*value++ = '\0';
+	}
+	kern_unsetenv(name);
+}
+
+
+#if 0
+static void g_wipe_eli_keys_consumer(struct g_consumer *cp) {
+	printf("Consumer: %s\n", cp->geom->name);
+}
+
+
+static void g_wipe_eli_keys_provider(struct g_provider *pp) {
+	printf("Provider: %s\n", pp->name);
+}
+#endif
+
+
+static void g_eli_zero_key(struct g_eli_softc *sc, struct g_eli_key *key) {
+	mtx_assert(&sc->sc_ekeys_lock, MA_OWNED);
+	explicit_bzero(key, G_ELI_DATAKEYLEN);
+}
+
+
+static void g_eli_my_key_destroy(struct g_eli_softc *sc) {
+	mtx_lock(&sc->sc_ekeys_lock);
+	if ((sc->sc_flags & G_ELI_FLAG_SINGLE_KEY) != 0) {
+		printf("Destroying single key!\n");
+		explicit_bzero(sc->sc_ekey, sizeof(sc->sc_ekey));
+	} else {
+		struct g_eli_key *key;
+
+		// printf("gek_next: 0x%x\n", TAILQ_FIRST(&sc->sc_ekeys_queue)->gek_next);
+
+		TAILQ_FOREACH(key, &sc->sc_ekeys_queue, gek_next) {
+			// printf("Destroying key: %lu!\n", key->gek_keyno);
+			g_eli_zero_key(sc, key);
+		}
+	}
+	mtx_unlock(&sc->sc_ekeys_lock);
+}
+
+
+static void g_wipe_eli_keys_geom(struct g_geom *gp) {
+#if 0
+	struct g_consumer *cp;
+	struct g_provider *pp;
+#endif
+
+	printf("Wiping ELI keys for geom: %s\n", gp->name);
+
+#if 0
+	LIST_FOREACH(cp, &gp->consumer, consumer) {
+		g_wipe_eli_keys_consumer(cp);
+	}
+
+	LIST_FOREACH(pp, &gp->provider, provider) {
+		g_wipe_eli_keys_provider(pp);
+	}
+#endif
+
+	g_eli_my_key_destroy(gp->softc);
+}
+
+
+static void g_wipe_eli_keys_class(struct g_class *mp) {
+	struct g_geom *gp;
+
+	if (strcmp(mp->name, G_ELI_CLASS_NAME) != 0)
+		return;
+
+	LIST_FOREACH(gp, &mp->geom, geom)
+	    g_wipe_eli_keys_geom(gp);
+}
+
+
+static void g_wipe_eli_keys(void *p, int flag) {
+	struct g_class *mp;
+
+	KASSERT(flag != EV_CANCEL, ("g_wipe_eli_keys was cancelled"));
+	g_topology_assert();
+
+	LIST_FOREACH(mp, &g_classes, class) {
+		g_wipe_eli_keys_class(mp);
+	}
+}
+
+
+static void wipe_geli_keys() {
+	g_waitfor_event(g_wipe_eli_keys, NULL, M_WAITOK, NULL);
+}
+
+
+static void destroy_crypto_info() {
+	zero_and_unset_env("kern.geom.eli.passphrase");
+	zero_and_unset_env("kern.geom.eli.passphrase.from_tpm2.passphrase");
+	wipe_geli_keys();
+}
 
 
 static int mypanic(const char *msg) {
@@ -29,6 +138,7 @@ static int mypanic(const char *msg) {
 	for (loop = panic_reboot_wait_time * 10; loop > 0; --loop) {
 		DELAY(1000 * 100); /* 1/10th second */
 	}
+	destroy_crypto_info();
 	panic("%s\n", msg);
 }
 
@@ -114,7 +224,8 @@ static void tpm2_check_passphrase_marker(void *param) {
 	}
 
 	printf("Passphrase marker found and matching - we are done.\n");
-	kern_unsetenv("kern.geom.eli.passphrase.from_tpm2.passphrase");
+	zero_and_unset_env("kern.geom.eli.passphrase.from_tpm2.passphrase");
+	zero_and_unset_env("kern.geom.eli.passphrase");
 
 	error = kern_close(td, fd);
 	if (error) {
