988f10e49653c24d0175ac88e1282d02744eafb2
Stanislaw Adaszewski <s.adaszewski@gmail.com>
2021-11-29 21:58:00 +0100

    Housekeeping, remove unused/unnecessary code.

--- stand/efi/include/efitpm2.h
+++ stand/efi/include/efitpm2.h
@@ -1,11 +1,6 @@
 #ifndef _EFITPM2_H_
 #define _EFITPM2_H_
 
-#include <efi.h>
-
-EFI_STATUS tpm2_init();
-EFI_STATUS tpm2_geli_passphrase_from_efivar();
-void tpm2_try_autoboot_or_clear_geli_keys();
 void tpm2_check_efivars();
 void tpm2_retrieve_passphrase();
 void tpm2_check_passphrase_marker();
